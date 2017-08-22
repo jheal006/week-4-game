eff7c6e1c406f6292a7f7bda7154ccdb75c24a1b
jheal006 <jheal006@gmail.com>
2017-08-21 23:44:49 -0700

    finished crystal game

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,18 +1,26 @@
+$(document).ready(function() {
 
-//Assign Varibles
+
+/////////////////
+//Variables//
+////////////////
 
 var wins = 0;
+    $("#wins").html(wins);
 var losses = 0;
-var totalScore;
+    $("#losses").html(losses);
+var totalScore = 0;
+    $("#score").html("<h1>Score:<br>" + totalScore + "</h1>");
+
+
+// Random Number Assigned to Dark Crystal Value
+var darkCrystalValue = Math.floor(Math.random() * 101 + 19);
+$("#darkCrystal").html("<h1>" + darkCrystalValue +"</h1>");
 
 var redCrystalValue = getColorCrystalValue();
 var blueCrystalValue = getColorCrystalValue();
 var yellowCrystalValue = getColorCrystalValue();
 var greenCrystalValue = getColorCrystalValue();
-var combinedCrystals = [];
-var crystalTotal;
-var darkCrystalValue = getDarkCrystaValue();
-
 
 /////////////////
 //Functions//
@@ -21,71 +29,83 @@ var darkCrystalValue = getDarkCrystaValue();
 //Get Random Values for the Different Colored Crystals
 
 function getColorCrystalValue () {
-  // min = Math.ceil(min);
-  // max = Math.floor(max);
-  return Math.floor(Math.random() * (Math.floor(12) - Math.ceil(1) + 1)) + Math.ceil(1);
+  return Math.floor((Math.random() * 11)+ 1);
 };
 
-function getDarkCrystaValue() {
-  document.getElementById("darkCrystal").innerHTML = Math.floor(Math.random() * (Math.floor(120) - Math.ceil(19) + 1)) + Math.ceil(19);
-};
+////////////
+//GAME//
+////////////
 
-//Math and functions for Combining Different Color Crystals
-
-function getSum(total, num) {
-  return total + num;
-};
-
-function crystalCount() {
-    crystalTotal = combinedCrystals.reduce(getSum);
-    document.getElementById("crystalValue").innerHTML = crystalTotal;
-};
-
-
-// Take Crystal Button and Value of Crystal Color and push to
-function crystalButtonClick (crystalColor, colorCrystalValue) {
-  $(crystalColor).click(function() {
-    combinedCrystals.push(colorCrystalValue);
-    crystalCount();
-  });
-}
-
-$(document).ready(function() {
-
-  // Click Different Crystals and Sum their values to the CrystalCount
-
-  document.getElementById("wins").innerHTML = "Wins: " + wins;
-  document.getElementById("losses").innerHTML = "Losses: " + losses;
-
-  // $(".red").click(function(){
-  //   combinedCrystals.push(redCrystalValue);
-  //   crystalCount();
-  // });
-
-  crystalButtonClick(".red",redCrystalValue);
-  crystalButtonClick(".blue",blueCrystalValue);
-  crystalButtonClick(".yellow",yellowCrystalValue);
-  crystalButtonClick(".green",greenCrystalValue);
-
-
-  //Game Rules, if the Crystal Total adds up to the exact amount of the Dark Crystal the Game is won.
-  if (crystalTotal === darkCrystalValue) {
+  //Game Win Conditions and Reset, if the Crystal Total adds up to the exact amount of the Dark Crystal the Game is won. Reprint adjusted Win and Loss Values, and Reset Crytal Values
+  var reset = function () {
+  if (totalScore === darkCrystalValue) {
     wins += 1;
-    combinedCrystals = [];
-    getDarkCrystaValue();
-    getColorCrystalValue();
-  } else if (crystalTotal > darkCrystalValue) {
+      $("#wins").html(wins);
+        alert("The Dark Crystal Has Been Destroyed!");
+  }  else if (totalScore > darkCrystalValue) {
     losses +=1;
-    combinedCrystals = [];
-    getDarkCrystaValue();
-    getColorCrystalValue();
-  };
+      $("#losses").html(losses);
+        alert("The Skeksis Killed the Gelfling!!");
+  }
+  darkCrystalValue = Math.floor(Math.random() * 101 + 19);
+    $("#darkCrystal").html("<h1>" + darkCrystalValue +"</h1>");
+  totalScore = 0;
+      $("#score").html("<h1>Score:<br>" + totalScore + "</h1>");
+  redCrystalValue = getColorCrystalValue();
+  blueCrystalValue = getColorCrystalValue();
+  yellowCrystalValue = getColorCrystalValue();
+  greenCrystalValue = getColorCrystalValue();
+};
 
 
+  // On Crystal Click take Value of Color Crystal and Add to Total Score
+
+  //// Can't get this to function properly. With this method the doc is printing the score as if the button is clicked on page load and not on the button click event.
 
 
+  // $(".red").click(crystalClick(redCrystalValue));
+  // $(".blue").click(crystalClick(blueCrystalValue));
+  // $(".yellow").click(crystalClick(yellowCrystalValue));
+  // $(".green").click(crystalClick(greenCrystalValue));
 
+  // function crystalClick(colorCrystalValue) {
+  //   totalScore = totalScore + colorCrystalValue;
+  //   $("#score").html(totalScore);
+  //   if (totalScore >= darkCrystalValue) {
+  //     reset();
+  //   };
+  // };
 
+  //Same function as above but the Total Value stays hidden until the button click.
+
+  $(".red").click(function() {
+    totalScore = totalScore + redCrystalValue;
+    $("#score").html("<h1>Score:<br>" + totalScore + "</h1>");
+    if (totalScore >= darkCrystalValue){
+    reset();
+    };
+  });
+  $(".blue").click(function() {
+    totalScore = totalScore + blueCrystalValue;
+    $("#score").html("<h1>Score:<br>" + totalScore + "</h1>");
+    if (totalScore >= darkCrystalValue){
+    reset();
+    };
+  });
+  $(".yellow").click(function() {
+    totalScore = totalScore + yellowCrystalValue;
+    $("#score").html("<h1>Score:<br>" + totalScore + "</h1>");
+    if (totalScore >= darkCrystalValue) {
+      reset();
+    };
+  });
+  $(".green").click(function() {
+    totalScore = totalScore + greenCrystalValue;
+    $("#score").html("<h1>Score:<br>" + totalScore + "</h1>");
+    if (totalScore >= darkCrystalValue) {
+      reset();
+    };
+  });
 
 
 
